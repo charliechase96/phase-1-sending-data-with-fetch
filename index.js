b0db225627cdb440b5c8b2f0c79bb050932852ad
Charlie Gray <charliechasegray@gmail.com>
2023-02-20 18:40:51 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+        name: `${name}`,
+        email: `${email}`
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    })        
+       .then(function (response) {
+        return response.json();
+       })
+       .then(function (object) {
+        document.body.textContent = object.id;
+       })
+       .catch(function (error) {
+        document.body.textContent = error.message;
+       })
+    }
